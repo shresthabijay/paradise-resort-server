c18fda18405616c328cebd6619ce6caa0accd395
shresthabijay <sbijay777@gmail.com>
2019-01-19 00:59:42 -0500

    serving react build with express

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express=require("express")
 const bodyparser=require("body-parser")
 const cors=require("cors")
 const mainRouteHandler=require("./routes/mainRouteHandler")
+const path = require('path');
 
 const app=express()
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors())
 
@@ -11,6 +14,13 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
 app.use(mainRouteHandler)
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+
 
 module.exports=app
 
