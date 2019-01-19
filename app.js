f2dd987df309c0123a337344ff5f27d819324021
shresthabijay <sbijay777@gmail.com>
2019-01-19 12:05:51 +0545

    ads

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express=require("express")
 const bodyparser=require("body-parser")
 const cors=require("cors")
+const path=require("path")
 const mainRouteHandler=require("./routes/mainRouteHandler")
 
 const app=express()
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors())
 
@@ -11,6 +13,11 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
 app.use(mainRouteHandler)
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
 
 module.exports=app
 
